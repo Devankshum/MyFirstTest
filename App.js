51a3ff99b43445814411e4c0528c0103494fa007
Devankshu <devankshu@Devankshus-MacBook-Air.local>
2021-12-03 11:54:45 +0530

    My First Commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React,{useState,createContext,useMemo} from 'react';
+import {
+  Button,
+  View,
+  Text
+} from 'react-native';
+
+const App = () => {
+  
+  return (
+            <View>
+              <Text>Hello World</Text>
+            </View>            
+          )
+}
+
+export default App;
