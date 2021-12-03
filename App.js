eb806a3107c78596564301aa4591e1e29ef5be88
Devankshu <devankshu@Devankshus-MacBook-Air.local>
2021-12-03 12:35:07 +0530

    Third Commit

--- App.js
+++ App.js
@@ -8,15 +8,17 @@ import {
 const App = () => {
   
   const [value, SetValue] = useState({
-    counter : 1
+    counter : 1,
+    counter1 : 1
   })
 
-  const {counter} = value;
+  const {counter,counter1} = value;
 
   return (
             <View>
               <Text>Hello World</Text>
               <Text>Counter : {counter}</Text>
+              <Text>Counter1 : {counter1}</Text>
             </View>            
           )
 }
