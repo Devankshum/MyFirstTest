9edc704dfca9e62bf00bf89465b43b622ee15c8b
Devankshu <devankshu@Devankshus-MacBook-Air.local>
2021-12-03 12:41:45 +0530

    Fourth Commit

--- App.js
+++ App.js
@@ -9,16 +9,18 @@ const App = () => {
   
   const [value, SetValue] = useState({
     counter : 1,
-    counter1 : 1
+    counter1 : 1,
+    counter2 : 1
   })
 
-  const {counter,counter1} = value;
+  const {counter,counter1,counter2} = value;
 
   return (
             <View>
               <Text>Hello World</Text>
               <Text>Counter : {counter}</Text>
               <Text>Counter1 : {counter1}</Text>
+              <Text>Counter2 : {counter2}</Text>
             </View>            
           )
 }
---
+++ Dashboard.js
@@ -0,0 +1,17 @@
+import React,{useState} from 'react';
+import {
+  Button,
+  View,
+  Text
+} from 'react-native';
+
+const Dashboard = () => {
+
+  return (
+            <View>
+              <Text>Dashboard</Text>
+            </View>            
+          )
+}
+
+export default App;
